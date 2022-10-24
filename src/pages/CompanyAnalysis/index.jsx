7be899f6e0248759540b8ec223046358b4d0cd1d
fairywing <miyald123@gmail.com>
2022-10-24 16:35:30 +0900

    test

--- src/pages/CompanyAnalysis/index.jsx
+++ src/pages/CompanyAnalysis/index.jsx
@@ -3,15 +3,6 @@ import axios from 'axios';
 import { CompanyAnalysisContainer } from './styles';
 
 function CompanyAnalysis() {
-  const getPosts = () => {
-    axios.get(
-      'http://localhost:3001/posts'.then((res) => {
-        console.log(res);
-      }),
-    );
-  };
-  getPosts();
-
   return <CompanyAnalysisContainer>Company Analysis</CompanyAnalysisContainer>;
 }
 
