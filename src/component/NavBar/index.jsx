759b2b413973ab18cc3f95938b5235e3399965a7
hkm104 <hkm104@naver.com>
2022-10-27 11:47:19 +0900

    index

--- src/component/NavBar/index.jsx
+++ src/component/NavBar/index.jsx
@@ -22,43 +22,44 @@ const Sidebar = ({ children }) => {
       icon: <FaUserAlt />,
     },
   ];
-const NavBar = () => {
-  const navigate = useNavigate();
+  const NavBar = () => {
+    const navigate = useNavigate();
 
-  const [searchKey, setSearchKey] = useState('');
+    const [searchKey, setSearchKey] = useState('');
 
-  const onInputChange = useCallback((event) => {
-    setSearchKey(event.target.value);
-  }, []);
+    const onInputChange = useCallback((event) => {
+      setSearchKey(event.target.value);
+    }, []);
 
-  const handleOnClick = () => {
-    navigate('/company-analysis');
-  };
-  // 버튼에 적용할 클릭 이벤트 함수
+    const handleOnClick = () => {
+      navigate('/company-analysis');
+    };
+    // 버튼에 적용할 클릭 이벤트 함수
 
-  const handleOnKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      handleOnClick(); // Enter 입력이 되면 클릭 이벤트 실행
+    const handleOnKeyPress = (e) => {
+      if (e.key === 'Enter') {
+        handleOnClick(); // Enter 입력이 되면 클릭 이벤트 실행
+      }
+    };
+    const location = useLocation();
+    console.log(location.pathname);
+    if (window.location.pathname === '/') {
+      return null;
     }
-  };
-  const location = useLocation();
-  console.log(location.pathname);
-  if (window.location.pathname === '/') {
-    return null;
-  }
-  return (
-    <NavbarContainer1>
-      <Text1>Invsetment Vaccine</Text1>
+    return (
+      <NavbarContainer1>
+        <Text1>Invsetment Vaccine</Text1>
 
-      <Input
-        placeholder='기업명을 입력해주세요.'
-        onChange={onInputChange}
-        onKeyPress={handleOnKeyPress}
-      />
-      <SearchIcon>
-        <img className='searchicon' alt='search' src='/img/vector.svg' />
-      </SearchIcon>
-    </NavbarContainer1>
-  );
+        <Input
+          placeholder='기업명을 입력해주세요.'
+          onChange={onInputChange}
+          onKeyPress={handleOnKeyPress}
+        />
+        <SearchIcon>
+          <img className='searchicon' alt='search' src='/img/vector.svg' />
+        </SearchIcon>
+      </NavbarContainer1>
+    );
+  };
 };
 export default NavBar;
